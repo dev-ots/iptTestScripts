58e68b3bffe29ded3bdd10ea6f0633a5a5781e4e
HEMANTHESH <39907426+HEMANTHESH@users.noreply.github.com>
2018-07-12 11:49:03 +0530

    PROCUREMENTPO Detail Scripts

---
+++ ortusolis_automation/test/com/ortusolis/registration/PROCUREMENT.java
@@ -0,0 +1,28 @@
+package com.ortusolis.registration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+import com.ortusolis.pageobjectsPO.PROCUREMENTPO;
+import com.ortusolis.utilities.TestBase;
+
+public class PROCUREMENT extends TestBase
+{
+	PROCUREMENTPO ppo =new PROCUREMENTPO();
+	final Logger log =LoggerFactory.getLogger(getClass().getName()) ;
+	   @Test
+	   public void login() throws Exception
+	   {
+		   log.info(oJsConfig.getString("IPT_Login_URL"));
+			driver.get(oJsConfig.getString("IPT_Login_URL"));
+			ppo.ProcusrLocators();
+			ppo.ProcUserLogin();
+			//ppo.SearchShikeisho();
+			//ppo.SelectSupplier();
+			Thread.sleep(2000);
+			ppo.RFQ();
+			ppo.selectlanguage();
+			
+	   }
+}
